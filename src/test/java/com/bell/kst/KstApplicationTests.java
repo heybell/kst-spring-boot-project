86c37ebc437401445947c1827a3f9de25cb736ed
heybell <heybell1229@gmail.com>
2023-04-17 21:33:07 +0900

    프로젝트 초기 설정: 기본 의존성 추가 및 애플리케이션 설정

---
+++ src/test/java/com/bell/kst/KstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bell.kst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
